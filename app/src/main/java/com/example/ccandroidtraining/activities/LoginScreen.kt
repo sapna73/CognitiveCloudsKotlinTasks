922029c1079ab01d0d9391f163c6328f53cecfc3
sapna73 <sg.sapna73@gmail.com>
2021-09-27 20:28:21 +0530

    forget password, signup

--- app/src/main/java/com/example/ccandroidtraining/activities/LoginScreen.kt
+++ app/src/main/java/com/example/ccandroidtraining/activities/LoginScreen.kt
@@ -1,12 +1,20 @@
 package com.example.ccandroidtraining.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.ccandroidtraining.R
 
 class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
+
+        var buttonLogin = findViewById<TextView>(R.id.btnLogin)
+        buttonLogin.setOnClickListener(){
+            intent = Intent(this, UIWidgets::class.java)
+            startActivity(intent)
+        }
     }
 }
