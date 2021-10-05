2bbf4cf3337378208e4517647969c36de5fea587
sapna73 <sg.sapna73@gmail.com>
2021-10-05 23:03:07 +0530

    tab 3 added

--- app/src/main/java/com/example/ccandroidtraining/activities/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/ccandroidtraining/activities/ui/main/SectionsPagerAdapter.kt
@@ -9,7 +9,8 @@ import com.example.ccandroidtraining.R
 
 private val TAB_TITLES = arrayOf(
     R.string.tab_text_1,
-    R.string.tab_text_2
+    R.string.tab_text_2,
+    R.string.tab_text_3
 )
 
 /**
@@ -30,7 +31,7 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     }
 
     override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
+        // Show 3 total pages.
+        return 3
     }
 }
