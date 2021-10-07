bf397266f7467392e019f1fc49e542aaaa7a6628
sapna73 <sg.sapna73@gmail.com>
2021-10-07 13:08:27 +0530

    Recycler view

--- app/src/main/java/com/example/ccandroidtraining/activities/RecyclerActivity.kt
+++ app/src/main/java/com/example/ccandroidtraining/activities/RecyclerActivity.kt
@@ -2,11 +2,38 @@ package com.example.ccandroidtraining.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.ccandroidtraining.R
+import com.example.ccandroidtraining.adapter.RecyclerAdapter
+import com.example.ccandroidtraining.model.ItemsModel
 
 class RecyclerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler)
-    }
+
+            // getting the recyclerview by its id
+            val recyclerview = findViewById<RecyclerView>(R.id.recyclerView)
+
+            // this creates a vertical layout Manager
+            recyclerview.layoutManager = LinearLayoutManager(this)
+
+            // ArrayList of class ItemsViewModel
+            val data = ArrayList<ItemsModel>()
+
+            // This loop will create 20 Views containing
+            // the image with the count of view
+            for (i in 1..20) {
+                data.add(ItemsModel(R.drawable.ic_baseline_delete_24, "Item " + i))
+            }
+
+            // This will pass the ArrayList to our Adapter
+            val adapter = RecyclerAdapter(data)
+
+            // Setting the Adapter with the recyclerview
+            recyclerview.adapter = adapter
+
+        }
+
 }
--- app/src/main/java/com/example/ccandroidtraining/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/example/ccandroidtraining/adapter/RecyclerAdapter.kt
@@ -3,34 +3,45 @@ package com.example.ccandroidtraining.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ccandroidtraining.R
 import com.example.ccandroidtraining.model.ItemsModel
 
-class RecyclerAdapter (private val moviesList: List<ItemsModel>) : RecyclerView.Adapter<RecyclerAdapter.MyViewHolder>() {
-    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var title: TextView = view.findViewById<TextView>(R.id.title)
-        var year: TextView = view.findViewById<TextView>(R.id.year1)
-        var genre: TextView = view.findViewById<TextView>(R.id.genre1)
+class RecyclerAdapter(private val mList: List<ItemsModel>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
+        // create new views
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            // inflates the card_view_design view
+            // that is used to hold list item
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.items_list, parent, false)
+
+            return ViewHolder(view)
+        }
+
+        // binds the list items to a view
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+            val ItemsViewModel = mList[position]
+
+            // sets the image to the imageview from our itemHolder class
+            holder.imageView.setImageResource(ItemsViewModel.image)
+
+            // sets the text to the textview from our itemHolder class
+            holder.textView.text = ItemsViewModel.text
+
+        }
+
+        // return the number of the items in the list
+        override fun getItemCount(): Int {
+            return mList.size
+        }
+
+        // Holds the views for adding it to image and text
+        class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+            val imageView: ImageView = itemView.findViewById(R.id.imageview)
+            val textView: TextView = itemView.findViewById(R.id.textView)
+        }
     }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.items_list, parent, false)
-
-        return MyViewHolder(itemView)
-    }
-
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-//        val Movies = moviesList[position]
-//        holder.title.text = Movies.title
-//        holder.genre.text = Movies.genre
-//        holder.year.text = Movies.year
-    }
-
-    override fun getItemCount(): Int {
-        return moviesList.size
-    }
-}
